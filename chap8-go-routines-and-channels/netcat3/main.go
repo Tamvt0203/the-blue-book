7dd994e006c7c6d8de7f611d921b7a7d37a3c0ae
Tamvt0203 <tamvt0203@gmail.com>
2026-05-18 08:39:57 +0700

    feat: exercise 8.3

---
+++ chap8-go-routines-and-channels/netcat3/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8800")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		if _, err := io.Copy(os.Stdout, conn); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("done")
+		done <- struct{}{} //signal the main go routine
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.(*net.TCPConn).CloseWrite()
+	<-done
+}
+func mustCopy(dest io.Writer, src io.Reader) {
+	if _, err := io.Copy(dest, src); err != nil {
+		log.Fatal(err)
+	}
+}
