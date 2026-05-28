29a870b669ca523a3f46d9e3b109508e4283e614
Tamvt0203 <tamvt0203@gmail.com>
2026-05-28 23:10:40 +0700

    example: thumbnail

---
+++ chap8-go-routines-and-channels/thumbnail/thumbnail_test.go
@@ -0,0 +1,109 @@
+package thumbnail_test
+
+import (
+	"log"
+	"os"
+	"sync"
+
+	"github.com/Tamvt0203/the-blue-book/chap8-go-routines-and-channels/thumbnail"
+)
+
+func makeThumbnails(filenames []string) {
+	for _, f := range filenames {
+		if _, err := thumbnail.ImageFile(f); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+// Incorrect!
+func makeThumbnails2(filenames []string) {
+	for _, f := range filenames {
+		go thumbnail.ImageFile(f)
+	}
+}
+
+func makeThumbnails3(filenames []string) {
+	ch := make(chan int)
+	for _, f := range filenames {
+		go func(f string) {
+			thumbnail.ImageFile(f)
+			ch <- 1
+		}(f)
+	}
+	for range filenames {
+		<-ch
+	}
+}
+
+func makeThumbnails4(filenames []string) error {
+	errors := make(chan error)
+	for _, f := range filenames {
+		go func(f string) {
+			_, err := thumbnail.ImageFile(f)
+			errors <- err
+		}(f)
+	}
+	for range filenames {
+		if err := <-errors; err != nil {
+			return err // Bugs! Will return after the first error
+			// > other go routines will block if try to send to that channel
+			// > never terminate
+			// > Solution1: use buffered channel
+		}
+	}
+	return nil
+}
+
+// rmakeThumbnails5 returns the list of thumbnail file name and an error if any step failed
+func makeThumbnails5(filenames []string) (thumbfiles []string, err error) {
+	type item struct {
+		thumbfile string
+		err       error
+	}
+	ch := make(chan item, len(filenames))
+	for _, f := range filenames {
+		go func(f string) {
+			var it item
+			it.thumbfile, it.err = thumbnail.ImageFile(f)
+			ch <- it
+		}(f)
+	}
+	for range filenames {
+		it := <-ch
+		if it.err != nil {
+			return nil, it.err
+		}
+		thumbfiles = append(thumbfiles, it.thumbfile)
+	}
+	return thumbfiles, nil
+}
+
+// makeThumbanils6 create thumbnails for filename received from a channle
+// and returns total bytes of all thumbnails
+func makeThumbnails6(filenames <-chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup
+	for f := range filenames {
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			thumbnail, err := thumbnail.ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumbnail)
+			sizes <- info.Size()
+		}(f)
+	}
+	go func() {
+		wg.Wait()
+		close(sizes)
+	}()
+	var total int64
+	for sz := range sizes {
+		total += sz
+	}
+	return total
+}
