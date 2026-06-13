42daab2fe0db4b1aa80667dbba680a18aec4db0a
Tamvt0203 <tamvt0203@gmail.com>
2026-06-13 16:27:33 +0700

    example: countdown2

---
+++ chap8-go-routines-and-channels/countdown2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Println("Commencing countdown. Press anything to abort")
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		abort <- struct{}{}
+	}()
+	tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		select {
+		case <-tick:
+			fmt.Println(countdown)
+		case <-abort:
+			fmt.Println("Launch aborted")
+			return
+		}
+	}
+	fmt.Println("Rocket lanched")
+}
