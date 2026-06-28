67d332faf6f12fe75cc7de00314c578ebb4096f4
Tamvt0203 <tamvt0203@gmail.com>
2026-06-28 10:41:20 +0700

    example: du5 (du with cancelation)

---
+++ chap8-go-routines-and-channels/du5/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+type Root struct {
+	name   string
+	nfiles int64
+	nbytes int64
+}
+
+type FileSize struct {
+	rootIndex int
+	fileSize  int64
+}
+
+func walkDir(dir string, fileSize chan<- FileSize, wg *sync.WaitGroup, rootIndex int) {
+	defer wg.Done()
+	if cancelled() {
+		return
+	}
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subDir := filepath.Join(dir, entry.Name())
+			wg.Add(1)
+			walkDir(subDir, fileSize, wg, rootIndex)
+		} else {
+			entryInfo, _ := entry.Info()
+			rootFileSize := FileSize{
+				rootIndex: rootIndex,
+				fileSize:  entryInfo.Size(),
+			}
+			fileSize <- rootFileSize
+		}
+	}
+}
+func dirents(dir string) []os.DirEntry {
+	select {
+	case smp <- struct{}{}:
+	case <-done:
+		return nil
+	}
+	defer func() {
+		<-smp
+	}()
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
+	}
+	return entries
+}
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return false
+	default:
+		return false
+	}
+}
+
+var verbose = flag.Bool("v", false, "show verbose progress message")
+var smp = make(chan struct{}, 2)
+var done = make(chan struct{})
+
+func main() {
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+	rootsInfo := make([]Root, 0)
+
+	fileSize := make(chan FileSize)
+	var wg sync.WaitGroup
+	for index, root := range roots {
+		rootInfo := Root{
+			name:   root,
+			nfiles: 0,
+			nbytes: 0,
+		}
+		rootsInfo = append(rootsInfo, rootInfo)
+		wg.Add(1)
+		go walkDir(root, fileSize, &wg, index)
+	}
+	go func() {
+		wg.Wait()
+		close(fileSize)
+	}()
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(1000 * time.Microsecond)
+	}
+loop:
+	for {
+		select {
+		case <-done:
+			for range fileSize {
+				//DO NOTHING
+			}
+			panic(1)
+
+		case fileSize, ok := <-fileSize:
+			if !ok {
+				break loop //fileSize was closed
+			}
+			rootsInfo[fileSize.rootIndex].nfiles++
+			rootsInfo[fileSize.rootIndex].nbytes += fileSize.fileSize
+		case <-tick:
+			printDiskUsage(rootsInfo)
+		}
+	}
+	printDiskUsage(rootsInfo)
+
+}
+func printDiskUsage(rootsInfor []Root) {
+	for _, rootInfo := range rootsInfor {
+		fmt.Printf("Dir %s: %d files %.3f MB\n", rootInfo.name, rootInfo.nfiles, float64(rootInfo.nbytes)/1e6)
+
+	}
+}
